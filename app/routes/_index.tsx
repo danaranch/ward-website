bbf9f609c0ad7793e998c6ab28cf39df65ebc014
Brandon Kent <brandonk3nt@gmail.com>
2024-08-14 09:07:30 -0700

    a

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -10,7 +10,19 @@ export const meta: MetaFunction = () => {
 export default function Index() {
   return (
     <div className="w-full h-full">
-      <img src="/jesus_heals_lame_man.jpeg" alt="jesus" />
+      <img src="/img/bethseda.jpeg" alt="jesus" />
+      <img
+        key="title-logo"
+        src="/images/modern-logo.png"
+        className="zero-auto w-3/4 md:w-1/2"
+        alt="church-logo"
+      />,
+      <div key="title-credits" className="church-font text-center p-1 py-2">
+        <p className="font-md">Dana Ranch Ward</p>
+        <p className="font-lg">Sacrament Meeting</p>
+        <p className="font-md">August 19, 2024</p>
+        <hr className="h-px my-4 bg-gray-400 border-0" />
+      </div>
     </div>
   );
 }
