99c1810fd78cc6f7ba05f66ddaa258a93b8a5edd
Brandon Kent <brandonk3nt@gmail.com>
2024-08-14 09:48:56 -0700

    element order fix

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -15,13 +15,13 @@ export default function Index() {
         src="/img/modern-logo.png"
         className="m-auto w-3/4 md:w-1/2"
         alt="church-logo"
-      />,
+      />
       <div key="title-credits" className="church-font text-center p-1 py-2">
         <p className="text-2xl">Dana Ranch Ward</p>
         <p className="text-3xl">Sacrament Meeting</p>
         <p className="text-2xl">August 19, 2024</p>
-        <hr className="h-px my-4 bg-gray-400 border-0" />
       </div>
+      <hr className="h-px my-4 bg-gray-400 border-0" />
     </div>
   );
 }
