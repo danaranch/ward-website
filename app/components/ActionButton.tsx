4e6274e9d49bdcea2caa6d49d2164c407c9638c8
Brandon Kent <brandonk3nt@gmail.com>
2024-08-18 06:03:19 -0700

    services components

---
+++ app/components/ActionButton.tsx
@@ -0,0 +1,21 @@
+import LightRays from "./LightRays";
+
+type ActionButtonProps = {
+  link: string;
+  text: string;
+};
+
+export default function ActionButton({ link, text }: ActionButtonProps) {
+  return (
+    <div className="text-center py-2">
+      <a href={link} target="_blank" rel="noreferrer">
+        <button className="relative bg-transparent text-sky-600 font-semibold py-2 px-8 border-2 border-sky-600 rounded">
+          <span className="-z-10 absolute top-0" style={{ left: -1 }}>
+            <LightRays color="#0ea5e9" />
+          </span>
+          {text}
+        </button>
+      </a>
+    </div>
+  );
+}
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,6 @@
 import type { MetaFunction } from "@netlify/remix-runtime";
+import { Link } from "@remix-run/react";
+import ActionButton from "~/components/ActionButton";
 
 export const meta: MetaFunction = () => {
   return [
@@ -10,11 +12,7 @@ export const meta: MetaFunction = () => {
 export default function Index() {
   return (
     <div className="w-full h-full">
-      <img
-        src="/img/bethseda.jpeg"
-        className="pb-4"
-        alt="jesus"
-      />
+      <img src="/img/bethseda.jpeg" className="pb-4" alt="jesus" />
       <img
         src="/img/modern-logo.png"
         className="m-auto w-3/4 md:w-1/2"
@@ -25,6 +23,182 @@ export default function Index() {
         <p className="text-3xl">Sacrament Meeting</p>
         <p className="text-2xl">August 19, 2024</p>
       </div>
+
+      <div className="pt-4">
+        <ActionButton link="" text="Join via broadcast" />
+      </div>
+      <hr className="h-px my-8 bg-gray-400 border-0" />
+
+      <div id="services" className="mx-2">
+        <div className="py-2">
+          <div className="flex py-1">
+            <div className="flex-none px-1">Presiding</div>
+            <div className="flex-1 mb-1 border-b border-dashed border-stone-300">
+            </div>
+            <div className="flex-none px-1">Bishop Kent</div>
+          </div>
+
+          <div className="flex py-1">
+            <div className="flex-none px-1">Conducting</div>
+            <div className="flex-1 mb-1 border-b border-dashed border-stone-300">
+            </div>
+            <div className="flex-none px-1">
+              <Link to="/business">Matthew Hulse</Link>
+            </div>
+          </div>
+
+          <div className="flex py-1">
+            <div className="flex-none px-1">Organist</div>
+            <div className="flex-1 mb-1 border-b border-dashed border-stone-300">
+            </div>
+            <div className="flex-none px-1">Joe Capps</div>
+          </div>
+
+          <div className="flex py-1">
+            <div className="flex-none px-1">Chorister</div>
+            <div className="flex-1 mb-1 border-b border-dashed border-stone-300">
+            </div>
+            <div className="flex-none px-1">Joe Capps</div>
+          </div>
+        </div>
+
+        <div className="text-center py-4">
+          <em className="font-semibold">Welcome & Announcements</em>
+        </div>
+
+        <div className="py-2">
+          <div className="py-1">
+            <div className="flex">
+              <div className="flex-none px-1">Opening Hymn</div>
+              <div className="flex-1 mb-1 border-b border-dashed border-stone-300 height-18">
+              </div>
+              <div className="flex-none px-1 text-right">27</div>
+            </div>
+            <div className="text-center font-sm truncate zero-auto max-75">
+              <div className="italic">Hymn title</div>
+              {
+                /* {hymn.link
+            ? (
+              <a
+                href={hymn.link}
+                target="_blank"
+                rel="noreferrer"
+                className="font-medium text-sky-600 underline italic"
+              >
+                {hymn.title}
+              </a>
+            )
+            : <div className="italic">{hymn.title}</div>} */
+              }
+            </div>
+          </div>
+
+          <div className="flex py-1">
+            <div className="flex-none px-1">Invocation</div>
+            <div className="flex-1 mb-1 border-b border-dashed border-stone-300 height-18">
+            </div>
+            <div className="flex-none px-1">By Invitation</div>
+          </div>
+        </div>
+
+        <div className="text-center py-4">
+          <em className="font-semibold">Ward Business</em>
+        </div>
+
+        <div className="py-2">
+          <div className="py-1">
+            <div className="flex">
+              <div className="flex-none px-1">Sacrament Hymn</div>
+              <div className="flex-1 mb-1 border-b border-dashed border-stone-300 height-18">
+              </div>
+              <div className="flex-none px-1 text-right">27</div>
+            </div>
+            <div className="text-center font-sm truncate zero-auto max-75">
+              <div className="italic">Hymn title</div>
+              {
+                /* {hymn.link
+            ? (
+              <a
+                href={hymn.link}
+                target="_blank"
+                rel="noreferrer"
+                className="font-medium text-sky-600 underline italic"
+              >
+                {hymn.title}
+              </a>
+            )
+            : <div className="italic">{hymn.title}</div>} */
+              }
+            </div>
+          </div>
+        </div>
+
+        <div className="text-center py-4">
+          <em className="font-semibold">Administration of the Sacrament</em>
+        </div>
+        {
+          /* <div className="text-center py-4">
+        <em className="font-semibold">Bearing of Testimonies</em>
+      </div> */
+        }
+
+        <div className="py-2">
+          <div className="flex py-1">
+            <div className="flex-none px-1">Speaker</div>
+            <div className="flex-1 mb-1 border-b border-dashed border-stone-300 height-18">
+            </div>
+            <div className="flex-none px-1">Phoenix Bellessa</div>
+          </div>
+
+          <div className="flex py-1">
+            <div className="flex-none px-1">Speaker</div>
+            <div className="flex-1 mb-1 border-b border-dashed border-stone-300 height-18">
+            </div>
+            <div className="flex-none px-1">Elder Riches</div>
+          </div>
+
+          <div className="py-1">
+            <div className="flex">
+              <div className="flex-none px-1">Intermediate Hymn</div>
+              <div className="flex-1 mb-1 border-b border-dashed border-stone-300 height-18">
+              </div>
+              <div className="flex-none px-1 text-right">27</div>
+            </div>
+            <div className="text-center font-sm truncate zero-auto max-75">
+              <div className="italic">Hymn title</div>
+            </div>
+          </div>
+
+          <div className="flex py-1">
+            <div className="flex-none px-1">Speaker</div>
+            <div className="flex-1 mb-1 border-b border-dashed border-stone-300 height-18">
+            </div>
+            <div className="flex-none px-1">Elder Riches</div>
+          </div>
+        </div>
+
+        <div className="py-2">
+          <div className="py-1">
+            <div className="flex">
+              <div className="flex-none px-1">Closing Hymn</div>
+              <div className="flex-1 mb-1 border-b border-dashed border-stone-300 height-18">
+              </div>
+              <div className="flex-none px-1 text-right">27</div>
+            </div>
+            <div className="text-center font-sm truncate zero-auto max-75">
+              <div className="italic">Hymn title</div>
+            </div>
+          </div>
+
+          <div className="flex py-1">
+            <div className="flex-none px-1">Benediction</div>
+            <div className="flex-1 mb-1 border-b border-dashed border-stone-300 height-18">
+            </div>
+            <div className="flex-none px-1">By Invitation</div>
+          </div>
+        </div>
+      </div>
+
       <hr className="h-px my-8 bg-gray-400 border-0" />
     </div>
   );
