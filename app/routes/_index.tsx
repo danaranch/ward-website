ce83a2fff4e85c4c3266a72c4c90d25d531c5066
Nicholas Wallace <nicholaslwallace@gmail.com>
2024-09-22 08:44:54 -0700

    September 22 Initial

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -25,7 +25,7 @@ export default function Index() {
       <div key="title-credits" className="church-font text-center p-1 py-2">
         <p className="text-2xl">Dana Ranch Ward</p>
         <p className="text-3xl">Sacrament Meeting</p>
-        <p className="text-2xl">September 15, 2024</p>
+        <p className="text-2xl">September 22, 2024</p>
       </div>
 
       {
@@ -35,7 +35,7 @@ export default function Index() {
       }
       <div className="pt-4">
         <ActionButton
-          link="https://youtube.com/live/E-HLEFkqDTE?feature=share"
+          link="https://youtube.com/live/Em3-wwjtwtA?feature=share"
           text="Join via broadcast"
         />
       </div>
@@ -61,14 +61,14 @@ export default function Index() {
             <div className="flex-none px-1">Organist</div>
             <div className="flex-1 mb-1 border-b border-dashed border-stone-300">
             </div>
-            <div className="flex-none px-1">Whitney Kolstad</div>
+            <div className="flex-none px-1">Joe Capps</div>
           </div>
 
           <div className="flex py-1">
             <div className="flex-none px-1">Chorister</div>
             <div className="flex-1 mb-1 border-b border-dashed border-stone-300">
             </div>
-            <div className="flex-none px-1">Joe Capps</div>
+            <div className="flex-none px-1">Laurel Cronin</div>
           </div>
         </div>
 
@@ -81,14 +81,30 @@ export default function Index() {
           <em className="sticky text-lg font-semibold">Announcements</em>
           <ol className="list-decimal ml-5 sticky">
             <li className="py-1">
-              Tithing Declaration is underway, signup sheets are on the door by
-              the clerks office.
+              Elder Bednar will be a guest speaker at a fireside tonight for all
+              young adults at the Tempe Institute building. Fireside starts at
+              6pm.
             </li>
             <li>
-              Mission and Life Skills Prep is underway for all juniors and
-              seniors (young men and young women) in high school, as well as
-              those recently graduated from high school. It is held every 2nd
-              and 4th Sunday at 4pm in the Relief Society room.
+              General Conference is in 2 weeks, for more info please visit
+              churchofjesuschrist.org. Our Fast Sunday for October will be on
+              Oct 13th.
+            </li>
+            <li>
+              Our ward is in charge of cleaning the building for the rest of the
+              year, so be on the lookout for opportunities to help with those
+              efforts.
+            </li>
+            <li>
+              The Primary Program is next week on September 29th.
+            </li>
+            <li>
+              Tithing declaration will conclude at the end of November. To sign
+              up please use the signup sheets by the clerk's office.
+            </li>
+            <li>
+              Mission and Life Skills Prep for all Juniors and Seniors is held
+              every 2nd and 4th Sunday at 4pm in the Relief Society room.
             </li>
           </ol>
         </div>
@@ -107,10 +123,10 @@ export default function Index() {
               <div className="flex-none px-1">Opening Hymn</div>
               <div className="flex-1 mb-1 border-b border-dashed border-stone-300 height-18">
               </div>
-              <div className="flex-none px-1 text-right">229</div>
+              <div className="flex-none px-1 text-right">81</div>
             </div>
             <div className="text-center font-sm truncate zero-auto max-75">
-              <div className="italic">Today, While the Sun Shines</div>
+              <div className="italic">Press Forward Saints</div>
             </div>
           </div>
 
@@ -118,7 +134,7 @@ export default function Index() {
             <div className="flex-none px-1">Invocation</div>
             <div className="flex-1 mb-1 border-b border-dashed border-stone-300 height-18">
             </div>
-            <div className="flex-none px-1">Addie Collett</div>
+            <div className="flex-none px-1">Jeff Downs</div>
           </div>
         </div>
 
@@ -132,10 +148,10 @@ export default function Index() {
               <div className="flex-none px-1">Sacrament Hymn</div>
               <div className="flex-1 mb-1 border-b border-dashed border-stone-300 height-18">
               </div>
-              <div className="flex-none px-1 text-right">176</div>
+              <div className="flex-none px-1 text-right">175</div>
             </div>
             <div className="text-center font-sm truncate zero-auto max-75">
-              <div className="italic">Tis Sweet to Sing the Matchless Love</div>
+              <div className="italic">O God, the Eternal Father</div>
             </div>
           </div>
         </div>
@@ -157,17 +173,15 @@ export default function Index() {
             <div className="flex-none px-1">Speaker</div>
             <div className="flex-1 mb-1 border-b border-dashed border-stone-300 height-18">
             </div>
-            <div className="flex-none px-1">Matt Hulse</div>
+            <div className="flex-none px-1">Natalya Cox</div>
           </div>
 
-{
-          /*
           <div className="py-1">
             <div className="flex">
               <div className="flex-none px-1">Musical Number</div>
               <div className="flex-1 mb-1 border-b border-dashed border-stone-300 height-18">
               </div>
-              <div className="flex-none px-1 text-right">Kyle Huffaker</div>
+              <div className="flex-none px-1 text-right">Dillon and Tegan Grenko</div>
             </div>
             <div className="text-center font-sm truncate zero-auto max-75">
               <div className="italic">
@@ -175,8 +189,8 @@ export default function Index() {
               </div>
             </div>
           </div>
-          */
-}
+          {
+            /*
           <div className="flex py-1">
             <div className="flex-none px-1">Intermediate Hymn</div>
             <div className="flex-1 mb-1 border-b border-dashed border-stone-300 height-18">
@@ -186,12 +200,14 @@ export default function Index() {
           <div className="text-center font-sm truncate zero-auto max-75">
             <div className="italic">Master, the Tempest is Raging</div>
           </div>
+          */
+          }
 
           <div className="flex py-1">
             <div className="flex-none px-1">Speaker</div>
             <div className="flex-1 mb-1 border-b border-dashed border-stone-300 height-18">
             </div>
-            <div className="flex-none px-1">David Kolstad</div>
+            <div className="flex-none px-1">Shaeli Kersee</div>
           </div>
         </div>
 
@@ -201,18 +217,25 @@ export default function Index() {
               <div className="flex-none px-1">Closing Hymn</div>
               <div className="flex-1 mb-1 border-b border-dashed border-stone-300 height-18">
               </div>
-              <div className="flex-none px-1 text-right">123</div>
+              <div className="flex-none px-1 text-right">1001</div>
             </div>
             <div className="text-center font-sm truncate zero-auto max-75">
-              <div className="italic">Oh, May My Soul Commune with Thee</div>
+              <div className="italic">Come Thou Fount of Every Blessing</div>
             </div>
+          </div>
+
+          <div className="flex py-1">
+            <div className="flex-none px-1">Speaker</div>
+            <div className="flex-1 mb-1 border-b border-dashed border-stone-300 height-18">
+            </div>
+            <div className="flex-none px-1">Dillon Grenko</div>
           </div>
 
           <div className="flex py-1">
             <div className="flex-none px-1">Benediction</div>
             <div className="flex-1 mb-1 border-b border-dashed border-stone-300 height-18">
             </div>
-            <div className="flex-none px-1">Tyler Cornia</div>
+            <div className="flex-none px-1">Duane Gunn</div>
           </div>
         </div>
       </div>
