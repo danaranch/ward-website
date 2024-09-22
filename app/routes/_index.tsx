09134fbd4edef1588cedddc11a2902bf7cbf6b1e
Brandon Kent <10407384+brandonk3nt@users.noreply.github.com>
2024-09-22 13:08:47 -0700

    conducting update

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -54,7 +54,7 @@ export default function Index() {
             <div className="flex-none px-1">Conducting</div>
             <div className="flex-1 mb-1 border-b border-dashed border-stone-300">
             </div>
-            <div className="flex-none px-1">Bishop Kent</div>
+            <div className="flex-none px-1">Matthew Hulse</div>
           </div>
 
           <div className="flex py-1">
