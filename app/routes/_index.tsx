017f14e89e3f26a40896d00b3225d24f94b0a6e5
brandonk3nt <brandonk3nt@gmail.com>
2026-02-01 00:41:05 -0700

    n ew hero img

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -20,11 +20,7 @@ export default function Index() {
      * - switces hero img by month automatically
      */
     <div className="w-full h-full">
-      <img
-        src="/img/woman-at-the-well.jpg"
-        className="pb-4 m-auto"
-        alt="jesus"
-      />
+      <img src="/img/bethseda.jpeg" className="pb-4 m-auto" alt="jesus" />
       <img
         src="/img/modern-logo.png"
         className="m-auto w-3/4 md:w-1/2 xl:w-1/3"
